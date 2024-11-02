51217befae162115e75241bfb7eaf07ea1d08f61
Yusuf Çokal <134581731+yusuffcx@users.noreply.github.com>
2024-11-02 17:04:05 +0300

    added sideDrawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-//import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/tabs.dart';
 
 final theme = ThemeData(
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/meals.dart';
+import 'package:meals/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -53,6 +54,7 @@ class _TabsScreenState extends State<TabsScreen> {
     }
 
     return Scaffold(
+      drawer: const MainDrawer(),
       body: activePage,
       appBar: AppBar(
         title: Text(title),
